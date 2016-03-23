a8036700a367e171fb0645b9c1343e0e2ded9f9d
Arlen Burton and Andrew Russo <arlen@>
2016-03-23 16:49:28 -0700

    survey made, objectives completed, attempted to create functioning personalized message upon submit

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+
+  // $("input.hidden1").click(function() {
+  //   $("input#otherInputBox").addClass("hidden");
+  });
+  $("input#otherRadio").click(function() {
+    $("input#otherInputBox").toggleClass("hidden");
+  });
+
+$("button#surveySubmit").submit(function() {
+
+  var userName = return("input#nameBox")
+
+  var sayThanks = function()
+    alert{}
+
+  thankYou("Thank you, "+userName+", for completing this survey.  Have a nice day!")
+
+  $("input#nameBox").
+  alert("")
+}
+
+});
